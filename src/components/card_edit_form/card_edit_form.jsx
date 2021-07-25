3bea1c97e9cdcd5b74a60009e2a373364c56d5be
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-07-25 16:46:23 +0900

    handle state change on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -3,22 +3,70 @@ import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
     const {name, company, title, email, message, theme, fileName, fileURL} = card;
+    
+    const onChange = (event) => {
+        if (event.currentTarget == null) {
+            return;
+        }
+        event.preventDefault();
+        //event에 currentTarget이 있다면 브라우저에서 기본적인 이벤트를 처리하지 않도록한다.
+        updateCard({
+            ...card, //기존의 키와 value를 그대로 쓰면서
+            [event.currentTarget.name]: event.currentTarget.value,
+        })
+    };
+    const onSubmit = () => {
+        deleteCard(card);
+    };
 
-    const onSubmit = () => { };
     return (
         <form className={styles.form}>
-            <input className={styles.input} type="text" name="name" value={name} />
-            <input className={styles.input} type="text" name="company" value={company} />
-            <select className={styles.select} name="theme" value={theme}>
+            <input
+                className={styles.input}
+                type="text"
+                name="name"
+                value={name}
+                onChange={onChange}
+            />
+            <input
+                className={styles.input}
+                type="text"
+                name="company"
+                value={company}
+                onChange={onChange}
+            />
+            <select
+                className={styles.select}
+                name="theme"
+                value={theme}
+            >
                 <option value="light">light</option>
                 <option value="dark">dark</option>
                 <option value="colorful">colorful</option>
             </select>
-            <input className={styles.input} type="text" name="title" value={title} />
-            <input className={styles.input} type="text" name="email" value={email} />
-            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <input
+                className={styles.input}
+                type="text"
+                name="title"
+                value={title}
+                onChange={onChange}
+            />
+            <input
+                className={styles.input}
+                type="text"
+                name="email"
+                value={email}
+                onChange={onChange}
+            />
+            <textarea
+                className={styles.textarea}
+                name="message"
+                value={message}
+                onChange={onChange}
+            >   
+            </textarea>
             <div className={styles.fileInput}>
                 <ImageFileInput />
             </div>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,12 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-    const [cards, setCards] = useState([
-        {
+    const [cards, setCards] = useState({
+        //배열이 아닌 오브젝트 형태로 관리하기 
+        // *배열도 가능하지만, 배열로 하면 맵핑할 때 시간이 오래 걸릴 수도 있으니까 오브젝트를 이용하면 빠르게 가능
+        //cards의 map이 아닌 Object.keys(cards)의 key로 가져올 수 있다
+
+        '1':{
             id: '1',
             name: 'Anna',
             company: 'Samsung',
@@ -19,7 +23,7 @@ const Maker = ({ authService }) => {
             fileName: 'anna',
             fileURL : null
         },
-        {
+        '2': {
             id: '2',
             name: 'Christal',
             company: 'Samsung',
@@ -30,7 +34,7 @@ const Maker = ({ authService }) => {
             fileName: 'christal',
             fileURL : null
         },
-        {
+        '3': {
             id: '3',
             name: 'Minsu',
             company: 'Samsung',
@@ -40,8 +44,11 @@ const Maker = ({ authService }) => {
             message: 'go for it',
             fileName: 'minsu',
             fileURL : 'minsu.png'
-        }
-    ]);
+    },
+    });
+  
+        
+    
     const history = useHistory();
 
     const onLogout = () => {
@@ -56,16 +63,28 @@ const Maker = ({ authService }) => {
         });
     });
 
-    const addCard = (card) => {
-        const updated = [...cards, card];
-        setCards(updated);
+    //add와 update를 한번에 해결할 수 있음
+    const createOrUpdateCard = (card) => {
+        setCards(cards => { //바로 이 시점의 setCards의 상태를
+            const updated = { ...cards }; //다 복사하고,
+            updated[card.id] = card; //card의 Object안에 있는 키를 이용해서 해당하는 키의 업데이트되는 card를 변경해서
+            return updated; //업데이트된 것으로 return!!
+        }); 
+    };
+
+    const deleteCard = (card) => {
+        setCards(cards => { //바로 이 시점의 setCards의 상태를
+            const updated = { ...cards }; //다 복사하고,
+            delete updated[card.id]; //delete 선언
+            return updated; //업데이트된 것으로 return!!
+        }); 
     };
 
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor cards={cards} addCard={addCard} />
+                <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
                 <Preview cards={cards} />
             </div>
             <Footer />
