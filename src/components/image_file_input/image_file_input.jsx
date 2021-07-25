fa5d47d656e19e35491da0eee87ba42b8cac2485
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-07-26 02:43:40 +0900

    add loading spinner for file input

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+    const [loading, setLoading] = useState(false); //업로드 로딩 우선 false로 설정
+
     const inputRef = useRef();
     const onButtonClick = (event) => {
         event.preventDefault(); //버튼을 누르면 subnit처럼 리프레쉬되기 때문에 방지하기 위해 사용
@@ -9,13 +11,14 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
     };
 
     const onChange = async event => {
-        console.log(event.target.files[0]);
+        setLoading(true); //event 받아오면 업로드 로딩 실행
         const uploaded = await imageUploader.upload(event.target.files[0]); //upload될 때 까지 기다렸다가
-        console.log(uploaded);
-         onFileChange({
-             name: uploaded.original_filename,
-             url : uploaded.url,
-         })
+        setLoading(false); //업로드 완료시 false로 설정
+
+        onFileChange({
+            name: uploaded.original_filename,
+            url: uploaded.url,
+        });
     };
 
     return (<div className={styles.container}>
@@ -27,9 +30,13 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
             name="file"
             onChange={onChange}
         />
-        <button className={styles.button} onClick={onButtonClick}>
+        {!loading &&
+            (<button className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+            onClick={onButtonClick}
+        >
             {name || 'No file'}
-        </button>
+        </button>)} 
+        {loading && <div className={styles.loading}></div>}
     </div>
     );
 };
