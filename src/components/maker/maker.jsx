f5d98bf51b72d7776f213d63e534f6f12f5b1c06
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-07-26 16:20:44 +0900

    use firebase realtime database

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,59 +6,38 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
-const Maker = ({ FileInput, authService }) => {
-    const [cards, setCards] = useState({
-        //배열이 아닌 오브젝트 형태로 관리하기 
-        // *배열도 가능하지만, 배열로 하면 맵핑할 때 시간이 오래 걸릴 수도 있으니까 오브젝트를 이용하면 빠르게 가능
-        //cards의 map이 아닌 Object.keys(cards)의 key로 가져올 수 있다
-
-        '1':{
-            id: '1',
-            name: 'Anna',
-            company: 'Samsung',
-            theme: 'light',
-            title: 'software Engineer',
-            email: 'anna@gmail.com',
-            message: 'go for it',
-            fileName: 'anna',
-            fileURL : null
-        },
-        '2': {
-            id: '2',
-            name: 'Christal',
-            company: 'Samsung',
-            theme: 'dark',
-            title: 'software Engineer',
-            email: 'christal@gmail.com',
-            message: 'go for it',
-            fileName: 'christal',
-            fileURL : null
-        },
-        '3': {
-            id: '3',
-            name: 'Minsu',
-            company: 'Samsung',
-            theme: 'colorful',
-            title: 'software Engineer',
-            email: 'minsu@gmail.com',
-            message: 'go for it',
-            fileName: 'minsu',
-            fileURL : 'minsu.png'
-    },
-    });
+//배열이 아닌 오브젝트 형태로 관리하기 
+// *배열도 가능하지만, 배열로 하면 맵핑할 때 시간이 오래 걸릴 수도 있으니까 오브젝트를 이용하면 빠르게 가능
+//cards의 map이 아닌 Object.keys(cards)의 key로 가져올 수 있다
+const Maker = ({ FileInput, authService, cardRepository }) => {
+    const historyState = useHistory().state; //로그인을 통해 오면 컴포넌트가 있을 것
+    const [cards, setCards] = useState({});
+    const [userId, setUserId] = useState(historyState && historyState.id);
   
-        
-    
     const history = useHistory();
-
     const onLogout = () => {
         authService.logout();
     };
 
+    //마운트가 되었을 때 or 사용자의 아이디가 변경될 때 마다
+    useEffect(() => {
+        if (!userId) {
+            return ; //아이디가 없으면 하지 않음
+        }
+        const stopSync = cardRepository.syncCards(userId, cards => {
+            setCards(cards);
+        }) //컴포넌트가 unmount 되었을 때불필여한 네트워크 사용을 최소화해주는 것
+        return () => stopSync();
+    }, [userId]);
+
+    //로그인될 때
     useEffect(() => {
         authService.onAuthChange(user => {
-            if (!user) {
-                history.push('/'); //로그아웃하여 사용자가 아니라면 홈으로 이동
+            if (user) {
+                setUserId(user.uid);
+            } else {
+                history.push('/');
+                //로그아웃하여 사용자가 아니라면 홈으로 이동
             }
         });
     });
@@ -69,7 +48,8 @@ const Maker = ({ FileInput, authService }) => {
             const updated = { ...cards }; //다 복사하고,
             updated[card.id] = card; //card의 Object안에 있는 키를 이용해서 해당하는 키의 업데이트되는 card를 변경해서
             return updated; //업데이트된 것으로 return!!
-        }); 
+        });
+        cardRepository.saveCard(userId, card);//firebase에 데이터 등록
     };
 
     const deleteCard = (card) => {
@@ -77,7 +57,8 @@ const Maker = ({ FileInput, authService }) => {
             const updated = { ...cards }; //다 복사하고,
             delete updated[card.id]; //delete 선언
             return updated; //업데이트된 것으로 return!!
-        }); 
+        });
+        cardRepository.removeCard(userId, card); //firebase에 있는 데이터 지우기
     };
 
     return (
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './app';
 import AuthService from './service/auth_service';
 import ImageUploader from './service/image_uploader';
 import ImageFileInput from './components/image_file_input/image_file_input';
+import CardRepository from './service/card_repository';
 
 const authService = new AuthService();
 const imageUploader = new ImageUploader();
+const cardRepository = new CardRepository();
+
 const FileInput = props => (
   //확장성을 위해 한단계 더 감싼 컴포넌트 사용 
   //prop을 받고 그대로 전달해주고,
@@ -18,7 +21,11 @@ const FileInput = props => (
 //컴포넌트 prop의 경우 대문자로 시작 ex)FileInput={FileInput}
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} FileInput={FileInput} />
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
